d8c105199f96c9fd1e05113b8afba5fe177b130f
jieungo <jieungogogo@gmail.com>
2022-04-19 23:07:29 +0900

    Feat: firebase storage 연동

--- goalgoal/components/SetupProfile.js
+++ goalgoal/components/SetupProfile.js
@@ -1,37 +1,84 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Pressable, Platform, Image, ActivityIndicator} from 'react-native';
 import {signOut} from '../lib/auth';
 import {createUser, usersCollection} from '../lib/users';
 import {useUserContext} from '../contexts/UserContext';
 import BorderInput from './BorderInput';
 import CustomButton from './CustomButton';
+import { launchImageLibrary } from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
 
 function SetupProfile() {
   const [displayName, setDisplayName] = useState('');
   const navigation = useNavigation();
   const {setUser} = useUserContext();
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const {params} = useRoute();
   const {uid} = params || {};
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
+    setLoading(true);
+
+    let photoURL = null;
+
+    if (response) {
+        const asset = response.assets[0];
+        const extension = asset.fileName.split('.').pop();
+        const reference = storage().ref(`/profile/${uid}.${extension}`);
+
+        if (Platform.OS === 'android') {
+            await reference.putString(asset.base64, 'base64', {
+                contentType: asset.type,
+            });
+        } else {
+            await reference.putFile(asset.uri);
+        }
+
+        photoURL = response ? await reference.getDownloadURL() : null;
+    }
+
     const user = {
       id: uid,
       displayName,
-      photoURL: null,
+      photoURL,
     };
     createUser(user);
     setUser(user);
   };
+  
   const onCancel = () => {
     signOut();
     navigation.goBack();
   };
 
+  const onSelectImage = () => {
+      launchImageLibrary(
+          {
+              mediaType: 'photo',
+              maxWidth: 512,
+              maxHeight: 512,
+              includeBase64: Platform.OS === 'android',
+          },
+          (res) => {
+              if (res.didCancel) {
+                  return;
+              }
+              setResponse(res);
+          },
+      );
+  };
+
   return (
     <View style={styles.block}>
-      <View style={styles.circle} />
+    <Pressable onPress={onSelectImage}>
+        <Image
+            style={styles.circle}
+            source={ response ? {uri: response?.assets[0]?.uri} : require('../assets/images/user.png')}
+        />
+    </Pressable>
       <View style={styles.form}>
         <BorderInput
           placeholder="닉네임"
@@ -40,10 +87,14 @@ function SetupProfile() {
           onSubmitEditing={onSubmit}
           returnKeyType="next"
         />
-        <View style={styles.buttons}>
-          <CustomButton title="다음" onPress={onSubmit} hasMargin />
-          <CustomButton title="취소" onPress={onCancel} theme="secondary" />
-        </View>
+        {loading ? (
+            <ActivityIndicator size={32} color="#6200ee" style={styles.spinner}/>
+        ):(
+            <View style={styles.buttons}>
+                <CustomButton title="다음" onPress={onSubmit} hasMargin />
+                <CustomButton title="취소" onPress={onCancel} theme="secondary" />
+            </View>
+        )}
       </View>
     </View>
   );
