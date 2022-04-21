6937d1a6fe68638b36a97ae8e6655f20346b4f0e
jieungo <jieungogogo@gmail.com>
2022-04-21 13:37:49 +0900

    Feat: post 등록 및 카드 컴포넌트 생성

---
+++ goalgoal/components/PostCard.js
@@ -0,0 +1,91 @@
+import React,{useMemo} from 'react';
+import {View, StyleSheet, Text, Image, Pressable} from 'react-native';
+
+function PostCard({user, photoURL, description, createdAt, id}) {
+    const date = useMemo(
+        () => (createdAt ? new Date(createdAt._second * 1000) : new Date()),
+        [createdAt],
+    )
+
+    const onOpenProfile = () => {
+        //
+    }
+
+return (
+    <View style={styles.block}>
+        <View style={[styles.head, styles.paddingBlock]}>
+            <Pressable style={styles.profile} onPress={onOpenProfile}>
+                <Image source={
+                    user.photoURL
+                    ? {
+                        uri: user.photoURL,
+                    } : require('../assets/user.png')
+                }
+                    resizeMode="cover"
+                    style={styles.avatar}
+                />
+                <Text style={styles.displayName}>{user.displayName}</Text>
+            </Pressable>
+        </View>
+            <Image 
+                source={{uri: photoURL}}
+                resizeMethod="resize"
+                resizeMode="cover"
+                style={styles.image}
+            />
+            <View style={styles.paddingBlock}>
+                <Text style={styles.description}>{description}</Text>
+                <Text date={date} style={styles.date}>{date.toLocalString()}</Text>
+            </View>
+    </View>
+)
+}
+
+const styles = StyleSheet.create({
+    block: {
+        paddingTop: 16,
+        paddingBottom: 16,
+    },
+    avatar: {
+        width: 32,
+        height: 32,
+        borderRadius: 16,
+    },
+    paddingBlock: {
+        paddingHorizontal: 16,
+    },
+    head: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 16,
+    },
+    profile: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    displayName: {
+        lineHeight: 16,
+        fontSize: 16,
+        marginLeft: 8,
+        fontWeight: 'bold',
+    },
+    image: {
+        backgroundColor: '#bdbdbd',
+        width: '100%',
+        aspectRatio: 1,
+        marginBottom: 16,
+    },
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        marginBottom: 8,
+    },
+    date: {
+        color: '#757575',
+        fontSize: 12,
+        lineHeight: 18,
+    }
+})
+
+export default PostCard;
--- goalgoal/screens/UploadScreen.js
+++ goalgoal/screens/UploadScreen.js
@@ -1,31 +1,93 @@
-import { useRoute } from '@react-navigation/native';
-import React, {useEffect, useRef, useState} from 'react';
-import { StyleSheet, View, TextInput, Image, useWindowDimensions, TouchableOpacity, Text } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
+import { StyleSheet, View, TextInput, Image, useWindowDimensions, Animated, Keyboard, Platform, KeyboardAvoidingView } from 'react-native';
 import UseCamera from '../components/UseCamera';
+import IconRightButton from '../components/IconRightButton';
+import storage from '@react-native-firebase/storage';
+import {useUserContext} from '../contexts/UserContext';
+import {v4} from 'uuid';
+import {createPost} from '../lib/posts';
 
 function UploadScreen() {
     const route = useRoute();
     const {res} = route.params || {};
     const {width} = useWindowDimensions();
+    const animation = useRef(new Animated.Value(width)).current;
+    const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+    const [description, setDescription] = useState('');
+    const navigation = useNavigation();
+    const {user} = useUserContext();
+    
+    const onSubmit = useCallback(async() => {
+        navigation.pop();
+        const asset = res.assets[0];
+
+        const extension = asset.fileName.split('.').pop();
+        const reference = storage().ref(`/photo/${user.id}/${v4()}.${extension}`);
+        if (Platform.OS === 'android') {
+            await reference.putString(asset.base64, 'base64', {
+                contentType: asset.type,
+            })
+        } else {
+            await reference.putFile(asset.uri);
+        }
+        const photoURL = await reference.getDownloadURL();
+        await createPost({description, photoURL, user});
+    }, [res, user, description, navigation]);
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerRight: () => <IconRightButton onPress={onSubmit} name="check" />,
+        })
+    }, [navigation, onSubmit]);
+
+    useEffect(() => {
+        const didShow = Keyboard.addListener('keyboardDidShow', () => setIsKeyboardOpen(true));
+        const didHide = Keyboard.addListener('keyboardDidHide', () => setIsKeyboardOpen(false));
+
+        return () => {
+            didShow.remove();
+            didHide.remove();
+        }
+    }, []);
+
+    useEffect(() => {
+        Animated.timing(animation, {
+            toValue: isKeyboardOpen ? 0 : width,
+            useNativeDriver: false,
+            duration: 150,
+            delay: 100,
+        }).start();
+    }, [isKeyboardOpen, width, animation]);
 
     return (
-        <View style={styles.block}>
-            {res ? (
-                <Image
+        <KeyboardAvoidingView
+            behavior={Platform.select({ios: 'height'})}
+            style={styles.block}
+            keyboardVerticalOffset={Platform.select({
+                ios: 180,
+            })}
+        >
+            <View style={styles.block}>
+                {res ? (
+                    <Animated.Image
                     source={{uri: res.assets[0]?.uri}}
-                    style={[styles.image, {height: width}]}
+                    style={[styles.image, {height: animation}]}
                     resizeMode="cover"
-                />
-            ):(
-                <UseCamera />
-            )}
-            <TextInput
-                style={styles.input}
-                multiline={true}
-                placeholder="오늘의 소감을 적어주세요 :)"
-                textAlignVertical='top'
-            />
-        </View>
+                    />
+                    ):(
+                        <UseCamera />
+                        )}
+                <TextInput
+                    style={styles.input}
+                    multiline={true}
+                    placeholder="오늘의 소감을 적어주세요 :)"
+                    textAlignVertical='top'
+                    value={description}
+                    onChangeText={setDescription}
+                    />
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
@@ -39,7 +101,8 @@ const styles = StyleSheet.create({
         paddingBottom: 16,
         flex: 1,
         fontSize: 16,
-    }
+    },
+    image: {width: '100%'},
 })
 
 export default UploadScreen;
